4590677322058e7405a53f50fb25f13010de6c29
ajabber <menshchikov.m@bk.ru>
2016-04-07 11:48:05 +0300

    Create 0nl1ne.cc

---
+++ vcards/0nl1ne.cc
@@ -0,0 +1,34 @@
+<vcard>
+    <fn>
+      <text>0nl1ne.cc</text>
+    </fn>
+    <kind>
+      <text>application</text>
+    </kind>
+    <url>
+      <uri>http://0nl1ne.cc/</uri>
+    </url>
+    <note>
+      <text>0nl1ne Jabber Server</text>
+    </note>
+    <registration xmlns='urn:xmpp:vcard:registration:1'>
+      <uri>xmpp:ajabber.me</uri>
+    </registration>
+    <bday>
+      <date>2016</date>
+    </bday>
+    <adr>
+      <country>NL</country>
+    </adr>
+    <ca xmlns='urn:xmpp:vcard:ca:0'>
+      <name>GlobalSign</name>
+      <uri>https://www.globalsign.com/</uri>
+    </ca>
+    <name xmlns='jabber:iq:version'>Ejabberd</name>
+    <impp>
+      <uri>xmpp:admin@0nl1ne.cc</uri>
+    </impp>
+    <geo>
+      <uri>geo:51.53,4.7</uri>
+    </geo>
+</vcard>
